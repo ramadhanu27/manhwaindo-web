b7e00ab1ca847f06f261168e486c9ca25f89919d
ramadhanu27 <rhamadhanu2019@gmail.com>
2025-12-09 22:07:10 +0700

    up

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import TurnstileGate from "@/components/TurnstileGate";
+import { ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import TurnstileGate from "@/components/TurnstileGate";
 
 interface ClientLayoutProps {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 export default function ClientLayout({ children }: ClientLayoutProps) {
